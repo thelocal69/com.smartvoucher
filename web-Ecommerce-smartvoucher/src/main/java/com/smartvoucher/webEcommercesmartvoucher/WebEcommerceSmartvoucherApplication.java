a5db2a718a31ac28b3cc6650342ab682b6ade2e5
thelocal69 <trankhanh740@gmail.com>
2023-09-13 07:38:09 +0700

    first commit

---
+++ web-Ecommerce-smartvoucher/src/main/java/com/smartvoucher/webEcommercesmartvoucher/WebEcommerceSmartvoucherApplication.java
@@ -0,0 +1,13 @@
+package com.smartvoucher.webEcommercesmartvoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEcommerceSmartvoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEcommerceSmartvoucherApplication.class, args);
+	}
+
+}
