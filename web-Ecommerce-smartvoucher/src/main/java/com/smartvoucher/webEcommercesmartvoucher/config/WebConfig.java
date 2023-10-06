6c7adf692957b6aa27bd859ae2696b60fca8543c
Dinh Nhan <dinhnhan121224@gmail.com>
2023-10-07 01:45:04 +0700

    test conflict

--- web-Ecommerce-smartvoucher/src/main/java/com/smartvoucher/webEcommercesmartvoucher/config/WebConfig.java
+++ web-Ecommerce-smartvoucher/src/main/java/com/smartvoucher/webEcommercesmartvoucher/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.smartvoucher.webEcommercesmartvoucher.config;
+package com.smartvoucher.webEcommercesmartvoucher.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
