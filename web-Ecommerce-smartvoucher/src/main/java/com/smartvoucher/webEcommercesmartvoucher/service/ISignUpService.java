cb875b743fe561e96de9eb369e18c19a9ad84230
Dinh Nhan <dinhnhan121224@gmail.com>
2023-11-02 23:12:42 +0700

    update feature singup

--- web-Ecommerce-smartvoucher/src/main/java/com/smartvoucher/webEcommercesmartvoucher/service/ISignUpService.java
+++
@@ -1,8 +0,0 @@
-package com.smartvoucher.webEcommercesmartvoucher.service;
-
-import com.smartvoucher.webEcommercesmartvoucher.dto.SignUpDTO;
-import com.smartvoucher.webEcommercesmartvoucher.payload.ResponseObject;
-
-public interface ISignUpService {
-    ResponseObject SignUp(SignUpDTO signUpDTO);
-}
