4ce9f409ed73e739979f6096bfd31b5d22d86261
thelocal69 <trankhanh740@gmail.com>
2023-11-11 18:35:30 +0700

    new feature send email in account Sat 11 Nov

--- web-Ecommerce-smartvoucher/src/main/java/com/smartvoucher/webEcommercesmartvoucher/service/IUserService.java
+++ web-Ecommerce-smartvoucher/src/main/java/com/smartvoucher/webEcommercesmartvoucher/service/IUserService.java
@@ -1,8 +1,13 @@
 package com.smartvoucher.webEcommercesmartvoucher.service;
 
 import com.google.api.services.drive.model.File;
+import com.smartvoucher.webEcommercesmartvoucher.dto.UserDTO;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IUserService {
     File uploadUserImages(MultipartFile fileName);
+    List<UserDTO> getAllUser();
+    UserDTO getEmail(UserDTO userDTO);
 }
